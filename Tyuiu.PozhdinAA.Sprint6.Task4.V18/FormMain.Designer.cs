6be7229ba44549427ac947e72d97b0a50e648849
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-12-09 15:46:04 +0500

    Task4

---
+++ Tyuiu.PozhdinAA.Sprint6.Task4.V18/FormMain.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace Tyuiu.PozhdinAA.Sprint6.Task4.V18
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1_PAA = new System.Windows.Forms.Button();
+            this.panel1_PAA = new System.Windows.Forms.Panel();
+            this.groupBox3_PAA = new System.Windows.Forms.GroupBox();
+            this.textBoxStartStep_PAA = new System.Windows.Forms.TextBox();
+            this.textBoxStopStep_PAA = new System.Windows.Forms.TextBox();
+            this.label2_PAA = new System.Windows.Forms.Label();
+            this.label1_PAA = new System.Windows.Forms.Label();
+            this.button4_PAA = new System.Windows.Forms.Button();
+            this.buttonSave_PAA = new System.Windows.Forms.Button();
+            this.button2_PAA = new System.Windows.Forms.Button();
+            this.groupBox2_PAA = new System.Windows.Forms.GroupBox();
+            this.textBox2_PAA = new System.Windows.Forms.TextBox();
+            this.panel3_PAA = new System.Windows.Forms.Panel();
+            this.groupBox1_PAA = new System.Windows.Forms.GroupBox();
+            this.textBoxResult_PAA = new System.Windows.Forms.TextBox();
+            this.splitter1_PAA = new System.Windows.Forms.Splitter();
+            this.chartFunction_PAA = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2_PAA = new System.Windows.Forms.Panel();
+            this.panel1_PAA.SuspendLayout();
+            this.groupBox3_PAA.SuspendLayout();
+            this.groupBox2_PAA.SuspendLayout();
+            this.panel3_PAA.SuspendLayout();
+            this.groupBox1_PAA.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFunction_PAA)).BeginInit();
+            this.panel2_PAA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1_PAA
+            // 
+            this.button1_PAA.Location = new System.Drawing.Point(457, 278);
+            this.button1_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.button1_PAA.Name = "button1_PAA";
+            this.button1_PAA.Size = new System.Drawing.Size(56, 19);
+            this.button1_PAA.TabIndex = 1;
+            this.button1_PAA.Text = "button1_PAA";
+            this.button1_PAA.UseVisualStyleBackColor = true;
+            this.button1_PAA.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // panel1_PAA
+            // 
+            this.panel1_PAA.BackColor = System.Drawing.Color.Silver;
+            this.panel1_PAA.Controls.Add(this.groupBox3_PAA);
+            this.panel1_PAA.Controls.Add(this.button4_PAA);
+            this.panel1_PAA.Controls.Add(this.buttonSave_PAA);
+            this.panel1_PAA.Controls.Add(this.button2_PAA);
+            this.panel1_PAA.Controls.Add(this.groupBox2_PAA);
+            this.panel1_PAA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1_PAA.Location = new System.Drawing.Point(0, 0);
+            this.panel1_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1_PAA.Name = "panel1_PAA";
+            this.panel1_PAA.Size = new System.Drawing.Size(977, 86);
+            this.panel1_PAA.TabIndex = 2;
+            this.panel1_PAA.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox3_PAA
+            // 
+            this.groupBox3_PAA.Controls.Add(this.textBoxStartStep_PAA);
+            this.groupBox3_PAA.Controls.Add(this.textBoxStopStep_PAA);
+            this.groupBox3_PAA.Controls.Add(this.label2_PAA);
+            this.groupBox3_PAA.Controls.Add(this.label1_PAA);
+            this.groupBox3_PAA.Location = new System.Drawing.Point(446, 3);
+            this.groupBox3_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3_PAA.Name = "groupBox3_PAA";
+            this.groupBox3_PAA.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3_PAA.Size = new System.Drawing.Size(302, 83);
+            this.groupBox3_PAA.TabIndex = 4;
+            this.groupBox3_PAA.TabStop = false;
+            this.groupBox3_PAA.Text = "Ввод данных";
+            // 
+            // textBoxStartStep_PAA
+            // 
+            this.textBoxStartStep_PAA.Location = new System.Drawing.Point(10, 37);
+            this.textBoxStartStep_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxStartStep_PAA.Multiline = true;
+            this.textBoxStartStep_PAA.Name = "textBoxStartStep_PAA";
+            this.textBoxStartStep_PAA.Size = new System.Drawing.Size(115, 26);
+            this.textBoxStartStep_PAA.TabIndex = 5;
+            // 
+            // textBoxStopStep_PAA
+            // 
+            this.textBoxStopStep_PAA.Location = new System.Drawing.Point(166, 37);
+            this.textBoxStopStep_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxStopStep_PAA.Multiline = true;
+            this.textBoxStopStep_PAA.Name = "textBoxStopStep_PAA";
+            this.textBoxStopStep_PAA.Size = new System.Drawing.Size(115, 26);
+            this.textBoxStopStep_PAA.TabIndex = 4;
+            // 
+            // label2_PAA
+            // 
+            this.label2_PAA.AutoSize = true;
+            this.label2_PAA.Location = new System.Drawing.Point(181, 20);
+            this.label2_PAA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2_PAA.Name = "label2_PAA";
+            this.label2_PAA.Size = new System.Drawing.Size(69, 13);
+            this.label2_PAA.TabIndex = 3;
+            this.label2_PAA.Text = "Конец шага:";
+            // 
+            // label1_PAA
+            // 
+            this.label1_PAA.AutoSize = true;
+            this.label1_PAA.Location = new System.Drawing.Point(34, 20);
+            this.label1_PAA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1_PAA.Name = "label1_PAA";
+            this.label1_PAA.Size = new System.Drawing.Size(67, 13);
+            this.label1_PAA.TabIndex = 2;
+            this.label1_PAA.Text = "Старт шага:";
+            // 
+            // button4_PAA
+            // 
+            this.button4_PAA.BackColor = System.Drawing.Color.Lime;
+            this.button4_PAA.Location = new System.Drawing.Point(752, 11);
+            this.button4_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.button4_PAA.Name = "button4_PAA";
+            this.button4_PAA.Size = new System.Drawing.Size(76, 55);
+            this.button4_PAA.TabIndex = 3;
+            this.button4_PAA.Text = "Выполнить";
+            this.button4_PAA.UseVisualStyleBackColor = false;
+            this.button4_PAA.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // buttonSave_PAA
+            // 
+            this.buttonSave_PAA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonSave_PAA.Location = new System.Drawing.Point(832, 10);
+            this.buttonSave_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSave_PAA.Name = "buttonSave_PAA";
+            this.buttonSave_PAA.Size = new System.Drawing.Size(77, 55);
+            this.buttonSave_PAA.TabIndex = 2;
+            this.buttonSave_PAA.Text = "Сохранить";
+            this.buttonSave_PAA.UseVisualStyleBackColor = false;
+            this.buttonSave_PAA.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // button2_PAA
+            // 
+            this.button2_PAA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2_PAA.BackColor = System.Drawing.Color.Green;
+            this.button2_PAA.Location = new System.Drawing.Point(913, 10);
+            this.button2_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.button2_PAA.Name = "button2_PAA";
+            this.button2_PAA.Size = new System.Drawing.Size(62, 55);
+            this.button2_PAA.TabIndex = 1;
+            this.button2_PAA.Text = "Справка";
+            this.button2_PAA.UseVisualStyleBackColor = false;
+            this.button2_PAA.Click += new System.EventHandler(this.buttonHelp_Click_NI);
+            // 
+            // groupBox2_PAA
+            // 
+            this.groupBox2_PAA.Controls.Add(this.textBox2_PAA);
+            this.groupBox2_PAA.Location = new System.Drawing.Point(0, 3);
+            this.groupBox2_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2_PAA.Name = "groupBox2_PAA";
+            this.groupBox2_PAA.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2_PAA.Size = new System.Drawing.Size(440, 83);
+            this.groupBox2_PAA.TabIndex = 0;
+            this.groupBox2_PAA.TabStop = false;
+            this.groupBox2_PAA.Text = "Условие";
+            // 
+            // textBox2_PAA
+            // 
+            this.textBox2_PAA.BackColor = System.Drawing.Color.Silver;
+            this.textBox2_PAA.Location = new System.Drawing.Point(8, 17);
+            this.textBox2_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2_PAA.Multiline = true;
+            this.textBox2_PAA.Name = "textBox2_PAA";
+            this.textBox2_PAA.ReadOnly = true;
+            this.textBox2_PAA.Size = new System.Drawing.Size(428, 58);
+            this.textBox2_PAA.TabIndex = 0;
+            this.textBox2_PAA.Text = resources.GetString("textBox2_PAA.Text");
+            // 
+            // panel3_PAA
+            // 
+            this.panel3_PAA.BackColor = System.Drawing.Color.Gray;
+            this.panel3_PAA.Controls.Add(this.groupBox1_PAA);
+            this.panel3_PAA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3_PAA.Location = new System.Drawing.Point(0, 86);
+            this.panel3_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3_PAA.Name = "panel3_PAA";
+            this.panel3_PAA.Size = new System.Drawing.Size(196, 445);
+            this.panel3_PAA.TabIndex = 3;
+            // 
+            // groupBox1_PAA
+            // 
+            this.groupBox1_PAA.Controls.Add(this.textBoxResult_PAA);
+            this.groupBox1_PAA.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1_PAA.Name = "groupBox1_PAA";
+            this.groupBox1_PAA.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1_PAA.Size = new System.Drawing.Size(194, 445);
+            this.groupBox1_PAA.TabIndex = 0;
+            this.groupBox1_PAA.TabStop = false;
+            this.groupBox1_PAA.Text = "Вывод:";
+            // 
+            // textBoxResult_PAA
+            // 
+            this.textBoxResult_PAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxResult_PAA.Location = new System.Drawing.Point(2, 15);
+            this.textBoxResult_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxResult_PAA.Multiline = true;
+            this.textBoxResult_PAA.Name = "textBoxResult_PAA";
+            this.textBoxResult_PAA.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult_PAA.Size = new System.Drawing.Size(190, 428);
+            this.textBoxResult_PAA.TabIndex = 0;
+            // 
+            // splitter1_PAA
+            // 
+            this.splitter1_PAA.Location = new System.Drawing.Point(196, 86);
+            this.splitter1_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.splitter1_PAA.Name = "splitter1_PAA";
+            this.splitter1_PAA.Size = new System.Drawing.Size(8, 445);
+            this.splitter1_PAA.TabIndex = 4;
+            this.splitter1_PAA.TabStop = false;
+            this.splitter1_PAA.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitter1_SplitterMoved);
+            // 
+            // chartFunction_PAA
+            // 
+            this.chartFunction_PAA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chartFunction_PAA.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartFunction_PAA.Legends.Add(legend2);
+            this.chartFunction_PAA.Location = new System.Drawing.Point(202, 0);
+            this.chartFunction_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.chartFunction_PAA.Name = "chartFunction_PAA";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartFunction_PAA.Series.Add(series2);
+            this.chartFunction_PAA.Size = new System.Drawing.Size(775, 445);
+            this.chartFunction_PAA.TabIndex = 0;
+            this.chartFunction_PAA.Text = "chart1";
+            // 
+            // panel2_PAA
+            // 
+            this.panel2_PAA.BackColor = System.Drawing.Color.Gray;
+            this.panel2_PAA.Controls.Add(this.chartFunction_PAA);
+            this.panel2_PAA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2_PAA.Location = new System.Drawing.Point(0, 86);
+            this.panel2_PAA.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2_PAA.Name = "panel2_PAA";
+            this.panel2_PAA.Size = new System.Drawing.Size(977, 445);
+            this.panel2_PAA.TabIndex = 3;
+            this.panel2_PAA.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 531);
+            this.Controls.Add(this.splitter1_PAA);
+            this.Controls.Add(this.panel3_PAA);
+            this.Controls.Add(this.panel2_PAA);
+            this.Controls.Add(this.panel1_PAA);
+            this.Controls.Add(this.button1_PAA);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.Text = "Спринт 6 | Такс 4 | Вариант 18 | Пождин А.А";
+            this.panel1_PAA.ResumeLayout(false);
+            this.groupBox3_PAA.ResumeLayout(false);
+            this.groupBox3_PAA.PerformLayout();
+            this.groupBox2_PAA.ResumeLayout(false);
+            this.groupBox2_PAA.PerformLayout();
+            this.panel3_PAA.ResumeLayout(false);
+            this.groupBox1_PAA.ResumeLayout(false);
+            this.groupBox1_PAA.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFunction_PAA)).EndInit();
+            this.panel2_PAA.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1_PAA;
+        private System.Windows.Forms.Panel panel1_PAA;
+        private System.Windows.Forms.Panel panel3_PAA;
+        private System.Windows.Forms.GroupBox groupBox1_PAA;
+        private System.Windows.Forms.TextBox textBoxResult_PAA;
+        private System.Windows.Forms.Splitter splitter1_PAA;
+        private System.Windows.Forms.GroupBox groupBox3_PAA;
+        private System.Windows.Forms.Label label2_PAA;
+        private System.Windows.Forms.Label label1_PAA;
+        private System.Windows.Forms.Button button4_PAA;
+        private System.Windows.Forms.Button buttonSave_PAA;
+        private System.Windows.Forms.Button button2_PAA;
+        private System.Windows.Forms.GroupBox groupBox2_PAA;
+        private System.Windows.Forms.TextBox textBox2_PAA;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartFunction_PAA;
+        private System.Windows.Forms.Panel panel2_PAA;
+        private System.Windows.Forms.TextBox textBoxStartStep_PAA;
+        private System.Windows.Forms.TextBox textBoxStopStep_PAA;
+    }
+}
+
